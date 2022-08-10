989fd0822b77728cf7e53e221c884e26549d55e5
Edward Dickey <edward.dickey@ECDs-MacBook-Pro.local>
2022-08-10 11:16:15 -0600

    commit tree 4577f7e33552e6c2e844cc840fdd7fcdc1345b41

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(cards, event){
+    let thankYouCards = []
+    for(let i=0;i< cards.length; i++){
+        
+        thankYouCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+function countDown(start){
+    while(start>0){
+        console.log(start);
+        start -=1;
+    }
+    console.log(start);
+}
